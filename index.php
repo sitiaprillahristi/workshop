e47e44dcc7a63257d86d7fddb89eb597872dcfb8
sitiaprillahristi <siti2153@adisanggoro.onmicrosoft.com>
2022-04-04 13:46:35 +0700

    04042022

--- index.php
+++ index.php
@@ -1,32 +1,119 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Software Engeneering</title>
-    <link rel="stylesheet" href="style.css">
-    <link rel="stylesheet" href="css/bootstrap.min.css">
-</head>
-<body> 
-    <header>
-        <h1>We Trust, We Choose</h1>
-    </header>
-    <div class="container">
-        <div class="hero">
-            <h1>Hero boboiboy</h1>
-        </div>
-        <div class="content1">
-            <h1>Ini nanti isinya konten pertama</h1>
-        </div>
-        <div class="content2">
-            <h1>Ini isinya konten kedua</h1>
-        </div>
-        <div class="content3">
-            <h1>Ini konten tiga</h1>
-        </div>
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    </div>
-<footer> NAh ini kakinya</footer>
-</body>
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css">
+
+    <title>My Portfolio - Siti Aprillah Risti</title>
+  </head>
+  <body>
+    <header>
+        <h3 class="text-left">Hi, Welcome to my page</h3>
+    </header>
+    <section class="jumbotron text-center">
+        <img src="img/prof.jpg" class="rounded-circle img-thumbnail" alt="my photo">
+        <h1 class="display-5">Siti Aprillah Risti</h1>
+        <p class="lead">Pelajar | Aktivis Dakwah</p>
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#fff" fill-opacity="1" d="M0,64L48,64C96,64,192,64,288,96C384,128,480,192,576,229.3C672,267,768,277,864,240C960,203,1056,117,1152,85.3C1248,53,1344,75,1392,85.3L1440,96L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path></svg>
+    </section>
+
+    <section class="about">
+        <div class="container">
+            <div class="row text-center">
+                <div class="col">
+                    <h2>Tentang Saya</h2>
+                </div>
+            </div>
+            <div class="row justify-content-center fs-5 text-center mb-3">
+                <div class="col-md-4">
+                    <p>Sedang mengeyam pendidikan di SMK Adi Sanggoro jurusan Teknik Informatika. Siswa yang antusias dan bermotivasi tinggi dengan kemampuan kepemimpinan, inisiatif dan mencari tantangan baru.</p>
+                </div>
+                <div class="col-md-4">
+                    <p>Memiliki pribadi yang cepat berbaur dan belajar dengan cepat mempermudah beradaptasi dengan tim. Berpengalaman dalam berorganisasi internal maupun eksternal sekolah.</p>
+                </div>
+            </div>
+        </div>
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#d6edf6" fill-opacity="1" d="M0,128L60,144C120,160,240,192,360,176C480,160,600,96,720,85.3C840,75,960,117,1080,133.3C1200,149,1320,139,1380,133.3L1440,128L1440,320L1380,320C1320,320,1200,320,1080,320C960,320,840,320,720,320C600,320,480,320,360,320C240,320,120,320,60,320L0,320Z"></path></svg>
+    </section>
+
+    <section class="contact">
+        <div class="container">
+            <div class="row text-center mb-3">
+                <div class="col">
+                    <h2>Proyek Saya</h2>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-4 mb-3">
+                    <div class="card text-center">
+                        <img src="img/LIMA.png" class="card-img-top" alt="Anonymous Messages">
+                        <div class="card-body">
+                            <p class="card-text">Anonymous Messages PHP</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4 mb-3">
+                    <div class="card text-center">
+                        <img src="img/P.png" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <p class="card-text">Skincare Website PHP</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4 mb-3">
+                    <div class="card text-center">
+                        <img src="img/Tampilancalculator.png" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <p class="card-text">Subnet Calculator PHP</p>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#fff" fill-opacity="1" d="M0,256L48,256C96,256,192,256,288,234.7C384,213,480,171,576,144C672,117,768,107,864,112C960,117,1056,139,1152,149.3C1248,160,1344,160,1392,160L1440,160L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path></svg>
+    </section>
+
+    <section class="ask">
+        <div class="container">
+            <div class="row text-center mb-4">
+                <div class="col">
+                    <h2>Kontak Saya</h2>
+                </div>
+            </div>
+            <div class="row justify-content-center text-center">
+                <div class="col-md-4">
+                    <i class="bi bi-envelope-fill" style="font-size: 1.6rem; display: inline; "></i>
+                    <span class="font-weight-bold">  sitiaprillahristi@gmail.com</span>
+                </div>
+                <div class="col-md-4">
+                    <i class="bi bi-instagram" style="font-size: 1.6rem; display: inline; "></i>
+                    <span class="font-weight-bold"><a href="https://www.instagram.com/risaprllh/"> @risaprllh</a></span>
+                </div>
+                <div class="col-md-4">
+                <i class="bi bi-github" style="font-size: 1.6rem; display: inline; "></i>
+                <span class="font-weight-bold"><a href="https://github.com/sitiaprillahristi"> sitiaprillahristi</a></span>
+                </div>
+            </div>
+        </div>
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#309cbd" fill-opacity="1" d="M0,160L80,144C160,128,320,96,480,101.3C640,107,800,149,960,154.7C1120,160,1280,128,1360,112L1440,96L1440,320L1360,320C1280,320,1120,320,960,320C800,320,640,320,480,320C320,320,160,320,80,320L0,320Z"></path></svg>
+    </section>
+
+
+<footer class="text-center text-white fw-bold pb-5">
+<p>Made with love <a href="https://www.instagram.com/risaprllh/"> risaprllh</a> &copy 2022</p>
+</footer>
+ 
+
+    <!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-fQybjgWLrvvRgtW6bFlB7jaZrFsaBXjsOMm/tB9LTS58ONXgqbR9W8oWht/amnpF" crossorigin="anonymous"></script>
+
+  </body>
 </html>
